02f596edd1a5ce4b873ef2d1e3c304ec3716443b
Rahul Wasnik <1713322+FeurialBlack@users.noreply.github.com>
2021-06-24 00:14:25 +0530

    Update mainfile.sql

--- mainfile.sql
+++ mainfile.sql
@@ -38,5 +38,75 @@ select * from SDEPT;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+show tables;
 
+create table S(
+ `S#` varchar(20),
+ Sname varchar(20),
+ Status varchar(20),
+ City varchar(20)
+ );
+ 
+ show tables;
+ desc S;
+ 
+ insert into S (`S#`,Sname,Status,City) values ('S1','Steven','20','London');
+ select * from S;
+ 
+ insert into S (`S#`, Sname,Status,City) values (`S2`,'Eric','25','Athens');
+ insert into S (`S#`, Sname,Status,City) values (`S3`,'Ryan','10','Paris');
+ insert into S (`S#`, Sname,Status,City) values (`S4`,'Rob','28','Paris');
+ insert into S (`S#`, Sname,Status,City) values (`S5`,'Robbin','8','London');
+ insert into S (`S#`, Sname,Status,City) values (`S6`,'Gerrad','8','Athens');
+ 
+ show tables;
+ select * from S;
+ select `S#`,Sname from S;
+ select * from S where City = 'London';
+ select * from s where City = 'Paris' or City = 'Athens';
+ select * from s where Status <= 20;
+ select * from s where Status >= 20;
+ select * from s where City != 'London';
+ 
+ create table P(
+  `P#` Varchar(20),
+  Pname varchar(20),
+  Color Varchar(10),
+  Weight Varchar (20),
+  City char(10)
+  );
+  
+  desc P;
+  
+  create table J (
+   `J#` Varchar(20),
+   Jname Varchar(15),
+   City Varchar(20)
+   );
+   
+   create table SPJ (
+    `S#` varchar (20),
+    `P#` varchar (20),
+    `J#` varchar (20)
+    );
+    
+    select * from s;
+    show tables;
+    
+    insert into p(`p#`,Pname,Color,Weight,City) values ('P1','chasis','black','13','Paris');
+    insert into p(`p#`,Pname,Color,Weight,City) values ('P2','Bearing','black','14','London');
+    insert into p(`p#`,Pname,Color,Weight,City) values ('P3','Seat cover','white','15','London');
+    insert into p(`p#`,Pname,Color,Weight,City) values ('P4','color Spray','silver','10','Athens');
+    
+    select * from p;
+    select * from p where City = 'Athens';
+    select Pname,Color from p where City = 'London';
+    select * from p where Weight >= 12 and Weight <=14;
+    select City from s;
+    select Weight from p;
+    select Weight*100 from p;
+    select weight*100 as "Kilograms" from p;
+    select weight/100 as "Milligrams" from p;
+ 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
