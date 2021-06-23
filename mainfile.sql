6156fe93895c8b11a5a6c045884448d37b15b99c
Rahul Wasnik <1713322+FeurialBlack@users.noreply.github.com>
2021-06-23 11:58:54 +0530

    Create mainfile.sql

---
+++ mainfile.sql
@@ -0,0 +1,25 @@
+create database edac;
+show databases;
+use edac;
+
+create table SEMP (
+EMPNO CHAR(4), 
+EMPNAME CHAR(20), 
+BASIC FLOAT(9,2), 
+DEPTNO CHAR(2), 
+DEPTHEAD CHAR(4)
+);
+
+insert into SEMP (EMPNO,EMPNAME,BASIC,DEPTNO) values ( (0001, 'SUNIL', 6000, 10), (0002, 'HIREN', 8000, 20) );
+
+insert into SEMP values ( 
+0003,'ALI',4000,10,0001
+);
+
+insert into SEMP (EMPNO,EMPNAME,BASIC,DEPTHEAD) values( 0004,'GEORGE',6000,0002 );
+ 
+select * from SEMP;
+
+
+
+
