7c459c351390bdcd047095ee30455ce90b98ba73
Rahul Wasnik <1713322+FeurialBlack@users.noreply.github.com>
2021-07-01 22:29:45 +0530

    Update CRM backend Mini project.sql

--- CRM backend Mini project.sql
+++ CRM backend Mini project.sql
@@ -1,12 +1,9 @@
-
 show databases;
----------------------------------------------------- Creating Database -------------------------------------------------------------------------------------------------------
 create database if not exists CRM;
 use CRM;
 
---------------------------------------------------- Creating Tables inside the Database ---------------------------------------------------------------------------------------
-
-create table if not exists Client(
+create table if not exists Client
+(
 Client_ID int primary key not null auto_increment,
 First_Name varchar(10) not null,
 Last_Name varchar(10) not null,
@@ -16,5 +13,18 @@ Email varchar(20) not null,
 Mob varchar(10) not null
 );
 
+desc client;
 
+create table if not exists Ticket(
+Ticket_id int primary key auto_increment,
+Client_id int not null,
+date datetime,
+Message text,
+Foreign key fk_ticket (Client_id)
+references client(Client_id)
+on update cascade
+on delete no action
 
+);
+
+desc Ticket;
