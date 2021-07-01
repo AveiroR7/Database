d6156dc4cc8c90949ebe802819d0acb282392dd9
Rahul Wasnik <1713322+FeurialBlack@users.noreply.github.com>
2021-07-01 15:46:55 +0530

    Update CRM backend Mini project.sql

--- CRM backend Mini project.sql
+++ CRM backend Mini project.sql
@@ -1,1 +1,20 @@
 
+show databases;
+---------------------------------------------------- Creating Database -------------------------------------------------------------------------------------------------------
+create database if not exists CRM;
+use CRM;
+
+--------------------------------------------------- Creating Tables inside the Database ---------------------------------------------------------------------------------------
+
+create table if not exists Client(
+Client_ID int primary key not null auto_increment,
+First_Name varchar(10) not null,
+Last_Name varchar(10) not null,
+Company varchar(10) not null,
+Location varchar(10) not null,
+Email varchar(20) not null,
+Mob varchar(10) not null
+);
+
+
+
