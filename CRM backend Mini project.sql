ce7b966149fc6b535ba23048d2277dab9dd280c8
Rahul Wasnik <1713322+FeurialBlack@users.noreply.github.com>
2021-07-01 23:25:06 +0530

    Update CRM backend Mini project.sql

--- CRM backend Mini project.sql
+++ CRM backend Mini project.sql
@@ -1,16 +1,17 @@
 show databases;
+
 create database if not exists CRM;
 use CRM;
 
 create table if not exists Client
 (
 Client_ID int primary key not null auto_increment,
-First_Name varchar(10) not null,
-Last_Name varchar(10) not null,
-Company varchar(10) not null,
-Location varchar(10) not null,
-Email varchar(20) not null,
-Mob varchar(10) not null
+First_Name varchar(50) not null,
+Last_Name varchar(50) not null,
+Company varchar(50) not null,
+Location varchar(50) not null,
+Email varchar(50) not null,
+Mob varchar(50) not null
 );
 
 desc client;
@@ -27,4 +28,17 @@ on delete no action
 
 );
 
+
 desc Ticket;
+
+delimiter //
+create procedure insert_client(First_Name varchar(50),Last_Name varchar(50),Company varchar(50),Location varchar(50),Email varchar(50),Mob varchar(50)) 
+begin
+	insert into Client (First_Name,Last_Name,Company,Location,Email,Mob) values (First_Name,Last_Name,Company,Location,Email,Mob);
+    commit;
+end //
+delimiter ;
+
+call insert_client('Rahul','Wasnik','Credie','Mumbai','r1@gmail','9920140848');
+call insert_client('Rahul','Bhilave','Google','Mumbai','r2@gmail','9920140348');
+select * from Client;
