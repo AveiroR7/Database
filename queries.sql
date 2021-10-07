e87faca992838f21d12a2675d2b03b3c710fa7bb
Rahul Wasnik <1713322+FeurialBlack@users.noreply.github.com>
2021-10-08 02:19:08 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -7,3 +7,14 @@ select * from customers where points < (select max(points) from customers) order
 select * from customers order by points  desc limit 1,4;
 
 select max(points) from customers where points not in (select max(points) from customers);
+
+
+
+// JOINS
+
+SELF JOIN---
+select a.FIRST_NAME, b.EMPLOYEE_ID
+from employees a, employees b
+where a.EMPLOYEE_ID < b.EMPLOYEE_ID;
+
+
